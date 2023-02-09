4aca51bc0c997ee3c7c37588a6467fa390d051c2
Hamid Asadi <hamid.19471@gmail.com>
2023-02-09 23:22:48 +0330

    Create: Create DB folder and fake product for use in app

---
+++ src/components/DB/Product.js
@@ -0,0 +1,28 @@
+const Product = [
+    {
+        id: 1,
+        name: "Product 1",
+        price: 100,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        name: "Product 2",
+        price: 200,
+        quantity: 1,
+    },
+    {
+        id: 3,
+        name: "Product 3",
+        price: 300,
+        quantity: 1,
+    },
+    {
+        id: 4,
+        name: "Product 4",
+        price: 400,
+        quantity: 1,
+    },
+];
+
+export default Product;

0b95acbab1d5b0b56b3961d94244dc78a0de2c08
Hamid Asadi <hamid.19471@gmail.com>
2023-02-10 02:03:13 +0330

    Create: Menu items data for use in header and navbar

---
+++ src/components/DB/MenuItems.jsx
@@ -0,0 +1,20 @@
+const MenuItems = [
+    {
+        id: 1,
+        title: "Home",
+    },
+    {
+        id: 2,
+        title: "About",
+    },
+    {
+        id: 3,
+        title: "Services",
+    },
+    {
+        id: 4,
+        title: "Contact",
+    },
+];
+
+export default MenuItems;

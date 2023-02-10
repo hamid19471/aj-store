1fe82aec59aba11762ac8f347a8520f340fb7a8d
Hamid Asadi <hamid.19471@gmail.com>
2023-02-11 01:35:04 +0330

    Add: Add Hero Section component to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import HeroSection from "./components/HeroSection/HeroSection";
 function App() {
     return (
         <div>
             <Header />
+            <HeroSection />
         </div>
     );
 }

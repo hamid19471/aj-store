97e70f7ec89c31d9f640d26bfa7812fedfd85c8a
Hamid Asadi <hamid.19471@gmail.com>
2023-02-11 01:36:51 +0330

    Update: add more style to hero section component

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import gift from "../../assets/images/gift.jpg";
+import handicraft from "../../assets/images/handicraft.jpg";
+import drink from "../../assets/images/drink-stuff.jpg";
+import divider from "../../assets/images/divider.png";
 
 const HeroSection = () => {
-    return <div>HeroSection</div>;
+    return (
+        <div>
+            <section className="container flex flex-col md:max-h-80 md:flex-row gap-3 mt-10">
+                <div className="flex flex-col gap-3">
+                    <div className="flex-1 flex gap-2 overflow-hidden rounded-md">
+                        <img
+                            src={gift}
+                            alt=""
+                            className="w-full h-full object-cover"
+                        />
+                    </div>
+                    <div className="flex-1 flex gap-2 overflow-hidden rounded-md">
+                        <img
+                            src={handicraft}
+                            alt=""
+                            className="hidden md:block w-full h-full object-cover"
+                        />
+                    </div>
+                </div>
+                <div className="flex gap-2 overflow-hidden rounded-md">
+                    <div className="flex-1">
+                        <img
+                            src={drink}
+                            alt=""
+                            className="hidden md:block w-full h-full object-cover"
+                        />
+                    </div>
+                </div>
+            </section>
+            <section className="hidden md:flex container mt-10">
+                <img src={divider} alt="" />
+            </section>
+        </div>
+    );
 };
 
 export default HeroSection;

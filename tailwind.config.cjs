deeab06d0c67d1e14426121625295f4660849256
Hamid Asadi <hamid.19471@gmail.com>
2023-02-11 01:35:36 +0330

    Update: Add flex-2 to extend in tailwind config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
             fontFamily: {
                 oswald: ["Oswald", "sans-serif"],
             },
+            flex: {
+                2: "2 2 0%",
+            },
         },
     },
     plugins: [],

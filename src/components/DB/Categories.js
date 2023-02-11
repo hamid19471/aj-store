42f9d4f5c10425b06f7ad6c705414433a6cae511
Hamid Asadi <hamid.19471@gmail.com>
2023-02-11 16:24:45 +0330

    Create: Create fake category for use in category section component

---
+++ src/components/DB/Categories.js
@@ -0,0 +1,53 @@
+import Serving from "../../assets/images/categorie-01.webp";
+import Drink from "../../assets/images/categorie-02.webp";
+import Bathroom from "../../assets/images/categorie-03.webp";
+import Decorative from "../../assets/images/categorie-04.webp";
+import Children from "../../assets/images/categorie-05.webp";
+import Luxury from "../../assets/images/categorie-06.webp";
+import Wall from "../../assets/images/categorie-07.webp";
+import Lighting from "../../assets/images/categorie-08.webp";
+
+const Categories = [
+    {
+        id: 1,
+        name: "Serving",
+        image: Serving,
+    },
+    {
+        id: 2,
+        name: "Drink",
+        image: Drink,
+    },
+    {
+        id: 3,
+        name: "Bathroom",
+        image: Bathroom,
+    },
+    {
+        id: 4,
+        name: "Decorative",
+        image: Decorative,
+    },
+    {
+        id: 5,
+        name: "Children",
+        image: Children,
+    },
+    {
+        id: 6,
+        name: "Luxury",
+        image: Luxury,
+    },
+    {
+        id: 7,
+        name: "Wall",
+        image: Wall,
+    },
+    {
+        id: 8,
+        name: "Lighting",
+        image: Lighting,
+    },
+];
+
+export default Categories;

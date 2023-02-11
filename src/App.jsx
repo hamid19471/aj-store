02225f239d3299e047fb306f0da33248ef31d219
Hamid Asadi <hamid.19471@gmail.com>
2023-02-11 16:24:14 +0330

    Update: import category section component to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CategorySection from "./components/CategorySection/CategorySection";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import HeroSection from "./components/HeroSection/HeroSection";
@@ -7,6 +8,7 @@ function App() {
         <div>
             <Header />
             <HeroSection />
+            <CategorySection />
             <Footer />
         </div>
     );
